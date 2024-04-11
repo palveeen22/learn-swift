c47f8b4b4abd75a67ff5058b069f97aa757defb6
Alvin Pratama <palveeen22@gmail.com>
2024-04-11 09:04:37 +0700

    feat: initial commit

---
+++ newApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newApp
+//
+//  Created by Alvin Putra Pratama on 11/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, guys")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newApp/newAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  newAppApp.swift
+//  newApp
+//
+//  Created by Alvin Putra Pratama on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct newAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
